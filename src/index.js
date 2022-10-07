52c6042335f2d8c5098dda552403a71a45c56963
Ayaf-Bless <blessambel1@gmail.com>
2022-10-07 18:17:43 +0300

    innilize the init function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
-const app = document.getElementById('app');
-app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
+const app = document.getElementById("app");
+app.innerHTML = "<h1>JavaScript HTML5 APIs</h1>";
+
+const init = function () {
+  console.log("Hello");
+};
+
+if ("draggable" in document.createElement("div")) {
+  init();
+}
